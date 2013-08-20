5f5f6942ff26a5ce8e6ff8541f85c2b42c39a8bd
Thierry Reding <treding@nvidia.com>
2013-08-20 16:33:33 +0200

    net: rtl8169: Add support for RTL8168evl/8111evl
    This chip is compatible with other RTL8168 chips and can be found on the
    NVIDIA Cardhu and Beaver boards.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/net/rtl8169.c
+++ drivers/net/rtl8169.c
@@ -247,6 +247,7 @@ static struct {
 	{"RTL-8168b/8111sb",	0x30, 0xff7e1880,},
 	{"RTL-8168b/8111sb",	0x38, 0xff7e1880,},
 	{"RTL-8168d/8111d",	0x28, 0xff7e1880,},
+	{"RTL-8168evl/8111evl",	0x2e, 0xff7e1880,},
 	{"RTL-8101e",		0x34, 0xff7e1880,},
 	{"RTL-8100e",		0x32, 0xff7e1880,},
 };
