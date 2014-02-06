73b4674866ee0e994d346f1c5b408d7b90319f40
Stephen Warren <swarren@wwwdotorg.org>
2014-02-05 22:12:24 -0700

    LOCAL: ARM: rpi: add script to chain-load U-Boot over DHCP/TFTP
    Signed-off-by: Stephen Warren <swarren@wwwdotorg.org>

--- include/configs/rpi-common.h
+++ include/configs/rpi-common.h
@@ -189,6 +189,7 @@
 #include <config_distro_bootcmd.h>
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
+	"dhcpuboot=usb start; dhcp u-boot.uimg; bootm\0" \
 	ENV_DEVICE_SETTINGS \
 	ENV_MEM_LAYOUT_SETTINGS \
 	BOOTENV
