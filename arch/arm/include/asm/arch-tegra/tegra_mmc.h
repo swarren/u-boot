wangxian0718/u-boot-1

mmc: tegra: port to standard clock/reset APIs
Tegra186 supports the new standard clock and reset APIs. Older Tegra SoCs
still use custom APIs. Enhance the Tegra MMC driver so that it can operate
with either set of APIs.

Signed-off-by: Stephen Warren <swarren@nvidia.com>

arch/arm/include/asm/arch-tegra/tegra_mmc.h
@@ -9,6 +9,11 @@
 #ifndef __TEGRA_MMC_H_
 #define __TEGRA_MMC_H_
 
+#include <common.h>
+#if defined(CONFIG_CLK) || defined(CONFIG_DM_RESET)
+#include <clk_client.h>
+#include <reset_client.h>
+#endif
 #include <fdtdec.h>
 #include <asm/gpio.h>
 
@@ -134,7 +139,10 @@ struct mmc_host {
 	int id;			/* device id/number, 0-3 */
 	int enabled;		/* 1 to enable, 0 to disable */
 	int width;		/* Bus Width, 1, 4 or 8 */
-#ifndef CONFIG_TEGRA186
+#if defined(CONFIG_CLK) || defined(CONFIG_DM_RESET)
+	struct reset_ctl reset_ctl;
+	struct clk clk;
+#else
 	enum periph_id mmc_id;	/* Peripheral ID: PERIPH_ID_... */
 #endif
 	struct gpio_desc cd_gpio;	/* Change Detect GPIO */

swarren/u-boot

arch/arm/include/asm/arch-tegra/tegra_mmc.h
@@ -9,6 +9,11 @@
 #ifndef __TEGRA_MMC_H_
 #define __TEGRA_MMC_H_
 
+#include <common.h>
+#if defined(CONFIG_CLK) || defined(CONFIG_DM_RESET)
+#include <clk.h>
+#include <reset.h>
+#endif
 #include <fdtdec.h>
 #include <asm/gpio.h>
 
@@ -134,7 +139,10 @@ struct mmc_host {
 	int id;			/* device id/number, 0-3 */
 	int enabled;		/* 1 to enable, 0 to disable */
 	int width;		/* Bus Width, 1, 4 or 8 */
-#ifndef CONFIG_TEGRA186
+#if defined(CONFIG_CLK) || defined(CONFIG_DM_RESET)
+	struct reset_ctl reset_ctl;
+	struct clk clk;
+#else
 	enum periph_id mmc_id;	/* Peripheral ID: PERIPH_ID_... */
 #endif
 	struct gpio_desc cd_gpio;	/* Change Detect GPIO */